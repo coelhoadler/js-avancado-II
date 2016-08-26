7f02e93dab3f9edc661de5314371ae0412b84663
Adler Coelho Santos <adler.coelho.santos@icloud.com>
2016-08-26 14:12:48 -0300

    Implementando função armadilha ao adicionar e esvaziar a Lista de Negociações

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,10 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        
+        // Arrow function tem o escopo léxico, seu this não é dinâmico
+        this._listaNegociacoes = new ListaNegociacoes(model =>
+            this._negociacoesView.update(model));
         
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
@@ -14,14 +17,11 @@ class NegociacaoController {
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);
-        
     }
     
     adiciona(event) {
-        
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
         
         this._mensagem.texto = 'Negociação adicionada com sucesso';
         this._mensagemView.update(this._mensagem);
@@ -29,6 +29,13 @@ class NegociacaoController {
         this._limpaFormulario();   
     }
     
+    apaga() {
+        this._listaNegociacoes.esvazia();
+
+        this._mensagem.texto = "Negociações apagadas com sucesso!";
+        this._mensagemView.update(this._mensagem);
+    }
+
     _criaNegociacao() {
         
         return new Negociacao(
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,17 +1,28 @@
 class ListaNegociacoes {
     
-    constructor() {
+    constructor(armadilha) {
         
         this._negociacoes = [];
+        this._armadilha = armadilha;
+        // this._contexto = contexto; // Contexto de execução
     }
     
     adiciona(negociacao) {
         
         this._negociacoes.push(negociacao);
+        this._armadilha(this);	
+        //Reflect.apply(this._armadilha, this._contexto, [this]); // Como se ele trocasse o .this atual pelo this._contexto passado
     }
     
     get negociacoes() {
         
         return [].concat(this._negociacoes);
     }
+
+    esvazia() {
+    	this._negociacoes = [];
+        this._armadilha(this);
+        //Reflect.apply(this._armadilha, this._contexto, [this]); 	
+    }
+
 }
